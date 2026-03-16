a60cccceb50f7d0b109d86b4685f52f80184989f
Makis <makis.arvin@gmail.com>
2026-03-16 09:39:39 -0700

    added dunst and fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,9 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+export PATH="$PATH:/home/jerry/.pesde/bin"
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
