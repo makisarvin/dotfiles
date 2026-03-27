7f2075a80560bda0eb96b191337cd90e9e6af9d9
Makis <makis.arvin@gmail.com>
2026-03-27 14:44:43 -0700

    added hyprbind and some theme configs

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo pacman -S qt5ct qt6ct kvantum breeze breeze-icons kvantum-qt5 nwg-look
+
+
+# install clip history
+pary -S cliphist wtype
+
+# install hyprbind.
+# a tool to show the keybindings
+wget https://github.com/ry2x/HyprBind/releases/download/v0.1.4/hyprbind-0.1.4-1-x86_64.pkg.tar.zst
+sudo pacman -U hyprbind-0.1.4-1-x86_64.pkg.tar.zst
+
